8c3030b63256e639028a7ceaa21e2f05410363dd
Abdul Sagheer <73389096+abdulsagheer@users.noreply.github.com>
2022-08-29 23:09:36 +0530

    Added cluster fork and some commands

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ if (cluster.isMaster) {
   // Cause index.js to be executed again but in child mode
   cluster.fork();
   // A cluster is a pool of similar workers running under a parent Node process.
-  // Workers are spawned using the fork() method of the child_processes module. 
-  // This means workers can share server handles and 
+  // Workers are spawned using the fork() method of the child_processes module.
+  // This means workers can share server handles and
   // use IPC (Inter-process communication) to communicate with the parent Node process
 } else {
   const express = require("express");
@@ -16,4 +16,14 @@ if (cluster.isMaster) {
   app.get("/", (req, res) => {
     res.send("Hi there");
   });
+
+  app.listen(5000);
+  // The cluster module provides a way of creating child processes that
+  // runs simultaneously and share the same server port.
+
+  // A cluster is a pool of similar workers running under a parent Node process.
+  // Worker Threads in Node.js is useful for performing heavy JavaScript tasks.
+  // With the help of threads, Worker makes it easy to run javascript codes in parallel making it much faster and efficient. We can do heavy tasks without even disturbing the main thread.
+  // Workers are generated using the fork() method of the child_processes module.
+  // This means workers can share server handles and use IPC (Inter-process communication) to communicate with the parent Node process
 }
